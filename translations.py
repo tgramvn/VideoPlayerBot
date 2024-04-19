31ddb43f9dceb14b22e3fc2e1d9cb58a81d2454d
tgramvn <109977670+tgramvn@users.noreply.github.com>
2024-04-20 01:35:56 +0700

    Update translations.py

--- translations.py
+++ translations.py
@@ -2,15 +2,15 @@ from config import ASSISTANT_NAME
 from helpers.bot_utils import BOT_NAME, USERNAME
 
 
-START_TEXT = f"👋🏻 **Hello**, \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Voice Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends 😉! \n\n**Made With ❤️ By @ImSafone!** 👑"
+START_TEXT = f"👋🏻 **Hello**, \n\nĐây là **{BOT_NAME}** \nTôi có thể truyền phát trực tiếp, radio, video YouTube và tệp âm thanh / video Telegram trên trò chuyện thoại của các nhóm Telegram. Hãy cùng thưởng thức chế độ xem điện ảnh của trình phát video nhóm với bạn bè của bạn 😉!** 👑"
 HELP_TEXT = f"""
 🛠-- **Setting Up Bot**:--
 
-\u2022 Start Voice Chat In Your Group!
-\u2022 Add Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) To Your Group!
-\u2022 Give Admin To Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) In Your Group!
+\u2022 Bắt đầu trò chuyện thoại trong nhóm của bạn!
+\u2022 Thêm tôi (@{USERNAME}) & trợ lý của tôi (@{ASSISTANT_NAME}) vào tới nhóm của bạn!
+\u2022 Trao quyền quản trị cho tôi (@{USERNAME}) & trợ lý của tôi (@{ASSISTANT_NAME}) trong nhóm của bạn!
 
-⚔️-- **Available Commands**:--
+⚔️-- **Các lệnh có sẵn**:--
 
 \u2022 `/play` - Stream An Audio
 \u2022 `/stream` - Stream An Video
@@ -19,4 +19,4 @@ HELP_TEXT = f"""
 \u2022 `/endstream` - End Stream & Left VC
 \u2022 `/restart` - Restart Bot (Sudo Only)
 """
-ABOUT_TEXT = f"💡-- **Information**:-- \n\nThis bot is created for streaming videos in telegram group video chats using several methods from WebRTC. Powered by pytgcalls the async client API for the Telegram Group Calls and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots. \n\n**This bot licensed under GNU-GPL 3.0 License!**"
+ABOUT_TEXT = f"💡-- **Information**:-- \n\nBot này được tạo để truyền phát video trong cuộc trò chuyện video nhóm telegram bằng một số phương pháp từ WebRTC. Được hỗ trợ bởi pytgcalls, API máy khách không đồng bộ cho Cuộc gọi nhóm Telegram và Pyrogram, Khung và Thư viện máy khách API MTProto của telegram trong Python thuần túy dành cho Người dùng và Bot. \n\n**Bot này được cấp phép theo Giấy phép GNU-GPL 3.0!**"
